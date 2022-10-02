dc5f156ad8b9effe94f8a1a98b574382613b7dfe
Gabriel antônio <programerxjeff@gmail.com>
2022-10-02 11:55:54 -0300

    exercicios python

--- Python/Exercicios/ex45.py
+++ Python/Exercicios/ex45.py
@@ -1,11 +1,46 @@
-import random
-print('''Suas opções
- [1] Pedra
- [2] Papel
- [3] Tesoura''')
+from random import randint
+import time
 
-escolha = int(input('Qual é a sua jogada? '))
-pc = randint(1, 3)
- 
- if escolha == 1:
-   print(f'')
+itens = ('Pedra', 'Papel', 'Tesoura')
+print('''Suas opções:
+ [0] Pedra
+ [1] Papel
+ [2] Tesoura''')
+pc = randint(0,2)
+player = int(input('Sua escolha: '))
+
+time.sleep(1)
+print('JO')
+time.sleep(1)
+print('KEN')
+time.sleep(1)
+print('PÔ!!')
+
+print(20*'-=')
+print('O jogador escolheu {}'.format(itens[player]))
+print('O computador escolheu {}'.format(itens[pc]))
+print(20*'-=')
+
+if pc == 0 :
+   if player == 0:
+      print('\033[33mEMPATE\033[m')
+   elif player == 2:
+      print('O jogador \033[31mPERDEU\033[m')
+   else:
+      print('O jogador \033[32mVENCEU\033[m')
+elif pc == 1:
+   if player == 1:
+      print('\033[33mEMPATE\033')
+   elif player == 0:
+      print('O jogador \033[31mPERDEU\033[m')
+   else:
+      print('O jogador \033[32mVENCEU\033[m')
+else:
+   if player == 2:
+      print('\033[33mEMPATE\033')
+   elif player == 1:
+      print('O jogador \033[31mPERDEU\033[m')
+   else:
+      print('O jogador \033[32mVENCEU\033[m')
+
+   
